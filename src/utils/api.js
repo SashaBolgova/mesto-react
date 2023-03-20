73d7817284cc4ceb20f6b2ff06b0aae3626473ef
SashaBolgova <113525092+SashaBolgova@users.noreply.github.com>
2023-03-20 13:04:28 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -94,10 +94,11 @@ class Api {
   
 }
 
-export const api = new Api({
+const api = new Api({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-59',
   headers: {
     authorization: '6e50be30-34f1-4e79-8a1d-cf176976ff81',
     'Content-Type': 'application/json'
   }
 }); 
+export default api
